f655f40d00e18b4c3323aaf48da7c2fde205e912
Jared Parsons <jared@paranoidcoding.org>
2026-05-12 09:16:29 -0700

    cleaned up the build

--- inspect_sdk.cs
+++
@@ -1,6 +0,0 @@
-using System;
-using System.Linq;
-using System.Reflection;
-var asm = typeof(GitHub.Copilot.SDK.CopilotClient).Assembly;
-foreach (var t in asm.GetExportedTypes().OrderBy(t => t.FullName))
-    Console.WriteLine(t.FullName);
